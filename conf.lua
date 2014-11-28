31e19a15b997766888b5237a0f4bc4d4c64b50b4
Seppi <seppi@josefnpat.com>
2014-11-28 16:24:30 -0500

    Moving the credits into the window title

---
+++ conf.lua
@@ -0,0 +1,6 @@
+function love.conf(t)
+  t.window.width = 320
+  t.window.height = 40
+  t.title = "ILoveMP3s Player by Iggyvolz - Licensed under the MIT license https://github.com/iggyvolz/ILoveMP3s"
+  t.window.resizable = true
+end
