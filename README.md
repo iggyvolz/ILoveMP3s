6653580677ec745ed94794bb6189dff9230b4781
iggyvolz <iggyvolz@gmail.com>
2014-11-28 15:05:40 -0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,14 @@
+ILoveMP3s - a minimalist MP3 player written in Love (a Lua framework).
+
+Controls:
+*Play/pause with space bar
+*Go to beginning of song with left arrow (goes to previous song if within first 5 seconds)
+*Go to next song with right arrow
+
+To package your mp3 files, put them all into assets.  Songs will be played in alphabetical order, so it is highly recommended that you include the song number at the beginning, like so: 01 - [Song Name].mp3
+
+To run directly from source, install [love](http://love2d.org) and run `love .`.
+
+To build .app's, .exe's, or .love's, please see [love's documentation](https://love2d.org/wiki/Game_Distribution).
+
+This software is licensed under the MIT license.  Please see the LICENSE file for more details.
---
+++ main.lua
@@ -0,0 +1,63 @@
+playlist={}
+currentSong=1
+loaded=false
+paused=false
+function timify(n)
+  n=math.floor(n)
+  m=math.floor(n/60)
+  s=n-(60*m)
+  return string.format("%02d:%02d",m,s)
+end
+function love.load(t)
+  for i,v in ipairs(love.filesystem.getDirectoryItems("assets")) do
+    if love.filesystem.isFile("assets/"..v) then
+      table.insert(playlist,{["name"]=v,["sound"]=love.audio.newSource("assets/"..v,"static")})
+    end
+  end
+  playlist[currentSong].sound:play()
+  loaded=true
+end
+function love.update(dt)
+  if not loaded then return end
+  if paused then return end
+  if playlist[currentSong].sound:isPlaying() then return end
+  if not playlist[currentSong] then
+    currentSong=1
+    paused=true
+    return
+  end
+  currentSong=currentSong+1
+  playlist[currentSong].sound:play()
+end
+function love.draw()
+  if not loaded then love.graphics.print("Loading...") end
+  if paused then
+    love.graphics.print(playlist[currentSong].name.." (paused)",0,0)
+  else
+    love.graphics.print(playlist[currentSong].name,0,0)
+  end
+  love.graphics.print(timify(playlist[currentSong].sound:tell()),0,10)
+  love.graphics.print("Play/Pause-Space  Next/Last Song-Left/Right",0,20)
+end
+function love.keypressed(k)
+  if k==" " then
+    if paused then
+      paused=false
+      playlist[currentSong].sound:play()
+    else
+      paused=true
+      playlist[currentSong].sound:pause()
+    end
+  elseif k=="right" then
+    playlist[currentSong].sound:stop()
+  elseif k=="left" then
+    if playlist[currentSong].sound:tell()<5 and currentSong ~= 1 then
+      playlist[currentSong].sound:stop()
+      currentSong=currentSong-1
+      playlist[currentSong].sound:play()
+    else
+      playlist[currentSong].sound:stop()
+      playlist[currentSong].sound:play()
+    end
+  end
+end
